3a867c4e45290600a438c17d311b24d82e60e98c
Ugness <33034982+Ugness@users.noreply.github.com>
2018-07-10 16:15:05 +0900

    Trainable
    TODO : train + hyperparameter tuning

---
+++ Pytorch/Dataset.py
@@ -0,0 +1,87 @@
+import os
+import skimage.io as io
+import torch.utils.data as data
+import torch.nn as nn
+import torchvision.transforms as transforms
+import numpy as np
+from PIL import Image
+
+
+class RandomCrop(object):
+    def __init__(self, size):
+        self.size = size
+
+    def __call__(self, sample):
+        img, mask = sample['image'], sample['mask']
+        img, mask = img.resize((256, 256), resample=Image.BILINEAR), mask.resize((256, 256), resample=Image.BILINEAR)
+        h, w = img.size
+        new_h, new_w = self.size, self.size
+
+        top = np.random.randint(0, h - new_h)
+        left = np.random.randint(0, w - new_w)
+        img = img.crop((left, top, left + new_w, top + new_h))
+        mask = mask.crop((left, top, left + new_w, top + new_h))
+
+        return {'image': img, 'mask': mask}
+
+
+class RandomFlip(object):
+    def __init__(self, prob):
+        self.prob = prob
+        self.flip = transforms.RandomHorizontalFlip(1.)
+
+    def __call__(self, sample):
+        if np.random.random_sample() < self.prob:
+            img, mask = sample['image'], sample['mask']
+            img = self.flip(img)
+            mask = self.flip(mask)
+            return {'image': img, 'mask': mask}
+        else:
+            return sample
+
+
+class ToTensor(object):
+    def __init__(self):
+        self.tensor = transforms.ToTensor()
+
+    def __call__(self, sample):
+        img, mask = sample['image'], sample['mask']
+        img, mask = self.tensor(img), self.tensor(mask)
+        return {'image': img, 'mask': mask}
+
+
+class DUTS_dataset(data.Dataset):
+    def __init__(self, root_dir, train=True, data_augmentation=True):
+        self.image_list = sorted(os.listdir('{}/DUTS-{}-Image'.format(root_dir, 'TR' if train else 'TE')))
+        self.mask_list = sorted(os.listdir('{}/DUTS-{}-Mask'.format(root_dir, 'TR' if train else 'TE')))
+        self.transform = transforms.Compose(
+            [RandomFlip(0.5),
+             RandomCrop(224),
+             ToTensor()])
+        self.root_dir = root_dir
+        self.train = train
+        self.data_augmentation = data_augmentation
+
+    def __len__(self):
+        return len(self.image_list)
+
+    def __getitem__(self, item):
+        img_name = '{}/DUTS-{}-Image/{}'.format(self.root_dir, 'TR' if self.train else 'TE', self.image_list[item])
+        mask_name = '{}/DUTS-{}-Mask/{}'.format(self.root_dir, 'TR' if self.train else 'TE', self.mask_list[item])
+        img = Image.open(img_name)
+        mask = Image.open(mask_name)
+        sample = {'image': img, 'mask': mask}
+
+        if self.data_augmentation:
+            sample = self.transform(sample)
+        return sample
+
+
+if __name__ == '__main__':
+    # os.chdir('..')
+    # print(os.getcwd())
+    ds = DUTS_dataset('../DUTS-TR')
+    pil = transforms.ToPILImage()
+    out = ds[0]
+    pil(out['image']).show()
+    pil(out['mask']).show()
---
+++ Pytorch/train.py
@@ -0,0 +1,59 @@
+import torch
+from torch.utils.data import DataLoader
+import torchvision
+from Pytorch.Network import Unet
+from Pytorch.Dataset import DUTS_dataset
+from tensorboardX import SummaryWriter
+import datetime
+import os
+
+
+cfg = {'PicaNet': "GGLLL",
+       'Size': [28, 28, 28, 56, 112, 224],
+       'Channel': [1024, 512, 512, 256, 128, 64],
+       'loss_ratio': [0.5, 0.5, 0.5, 0.8, 0.8, 1]}
+
+if __name__ == '__main__':
+    vgg = torchvision.models.vgg16(pretrained=True)
+    device = torch.device("cuda")
+    batch_size = 1
+    epoch = 20
+    dataset = DUTS_dataset('../DUTS-TR')
+    # noise = torch.randn((batch_size, 3, 224, 224)).type(torch.cuda.FloatTensor)
+    # target = torch.randn((batch_size, 1, 224, 224)).type(torch.cuda.FloatTensor)
+
+    # print(vgg.features(noise))
+    # print(model(noise))
+    # print(model.seq)
+    # print(vgg.features)
+    # print(F.mse_loss(model.seq[:8](noise), vgg.features[:8](noise)))
+    model = Unet(cfg).cuda()
+    model.encoder.seq.load_state_dict(vgg.features.state_dict())
+    learning_rate = 0.001
+    lr_decay = 0.1
+    decay_step = 7000
+    opt_en = torch.optim.SGD(model.encoder.parameters(), lr=learning_rate, momentum=0.9, weight_decay=0.0005)
+    opt_dec = torch.optim.SGD(model.decoder.parameters(), lr=learning_rate * 10, momentum=0.9, weight_decay=0.0005)
+    dataloader = DataLoader(dataset, batch_size)
+    now = datetime.datetime.now()
+    os.makedirs('log/{}'.format(now.strftime('%m%d%H%M')), exist_ok=True)
+    writer = SummaryWriter('log/{}'.format(now.strftime('%m%d%H%M')))
+    for epo in range(epoch):
+        for i, batch in enumerate(dataloader):
+            # print(batch['image'].size())
+            opt_dec.zero_grad()
+            opt_en.zero_grad()
+            img = batch['image'].to(device)
+            mask = batch['mask'].to(device)
+            pred, loss = model(img, mask)
+            loss.backward()
+            opt_dec.step()
+            opt_en.step()
+            writer.add_scalar('loss', float(loss), global_step= i + epo*len(dataloader))
+            if i % 10 == 0:
+                for masked in pred:
+                    writer.add_image('{}'.format(masked.size()[2]), masked, i)
+                writer.add_image('target', img, i)
+            if i % 1000 == 0:
+                os.makedirs('models/{}'.format(now.strftime('%m%d%H%M')), exist_ok=True)
+                torch.save(model, 'models/{}/{}epo_{}step.ckpt'.format(now.strftime('%m%d%H%M'), epo, i))
