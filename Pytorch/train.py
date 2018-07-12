Fix random seed. Clip_norm gradient

Pytorch/train.py
@@ -13,6 +13,8 @@ cfg = {'PicaNet': "GGLLL",
        'loss_ratio': [0.5, 0.5, 0.5, 0.8, 0.8, 1]}
 
 if __name__ == '__main__':
+    torch.cuda.manual_seed_all(1234)
+    torch.manual_seed(1234)
     vgg = torchvision.models.vgg16(pretrained=True)
     device = torch.device("cuda")
     batch_size = 1
@@ -33,6 +35,8 @@ if __name__ == '__main__':
     decay_step = 7000
     opt_en = torch.optim.SGD(model.encoder.parameters(), lr=learning_rate, momentum=0.9, weight_decay=0.0005)
     opt_dec = torch.optim.SGD(model.decoder.parameters(), lr=learning_rate * 10, momentum=0.9, weight_decay=0.0005)
+    # opt_en = torch.optim.Adam(model.encoder.parameters(), lr=learning_rate, weight_decay=0.0005)
+    # opt_dec = torch.optim.Adam(model.decoder.parameters(), lr=learning_rate * 10, weight_decay=0.0005)
     dataloader = DataLoader(dataset, batch_size, num_workers=0)
     now = datetime.datetime.now()
     os.makedirs('log/{}'.format(now.strftime('%m%d%H%M')), exist_ok=True)
@@ -47,20 +51,32 @@ if __name__ == '__main__':
             mask = batch['mask'].to(device)
             pred, loss = model(img, mask)
             loss.backward()
+            torch.nn.utils.clip_grad_norm_((model.parameters(), 100))
             opt_dec.step()
             opt_en.step()
             writer.add_scalar('loss', float(loss), global_step=i + epo * len(dataloader))
+
             if i % 10 == 0:
                 for masked in pred:
                     writer.add_image('{}'.format(masked.size()[2]), masked, i)
-                writer.add_image('target', img, i)
-            if i % 1000 == 0 and i != 0:
+                writer.add_image('GT', mask, i)
+                writer.add_image('Image', img, i)
+
+            if i % 200 == 0 and i != 0:
                 os.makedirs('models/{}'.format(now.strftime('%m%d%H%M')), exist_ok=True)
                 torch.save(model, 'models/{}/{}epo_{}step.ckpt'.format(now.strftime('%m%d%H%M'), epo, i))
+            if i % 1000 == 0:
+                check = os.listdir('models/{}'.format(now.strftime('%m%d%h%M')))
+                for file in check:
+                    if '00' in file and '000' not in file:
+                        os.remove('models/{}/{}'.format(now.strftime('%m%d%h%M'), file))
             if i + epo * len(dataloader) % decay_step == 0 and i != 0:
                 learning_rate *= lr_decay
                 opt_en = torch.optim.SGD(model.encoder.parameters(), lr=learning_rate, momentum=0.9,
                                          weight_decay=0.0005)
                 opt_dec = torch.optim.SGD(model.decoder.parameters(), lr=learning_rate * 10, momentum=0.9,
                                           weight_decay=0.0005)
+                # opt_en = torch.optim.Adam(model.encoder.parameters(), lr=learning_rate, weight_decay=0.0005)
+                # opt_dec = torch.optim.Adam(model.decoder.parameters(), lr=learning_rate * 10, weight_decay=0.0005)
+
             del loss